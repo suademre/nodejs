9238733f8f24ff46f11182f2afe028e7a4a0bf85
Suad Emre Umar <suadeumar@gmail.com>
2021-09-27 18:40:49 +0200

    added express-server

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-/* console.log('Hello World!')
-
-const http = require('http')
-
-const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hellooo</h1>')
-})
-
-server.listen(port, () => {
-  console.log(`es funktioniert im port ${port}`)
-})
- */
-
 const express = require('express')
 const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json())
+
+app.get('/api/cards', (req, res) => {
   res.set('Content-Type', 'text/html; charset=utf-8')
   res.send('<h1>Hello World! Wie Läuft</h1>')
   /* res.send('<h1>Hello World!</h1>') */
+})
+
+app.post('/api/cards', (req, res) => {
+  console.log(req.body)
+  res.send('This is post request')
+})
+
+app.put('/api/cards', (req, res) => {
+  res.send('This is the put request')
+})
+
+app.patch('/api/cards', (req, res) => {
+  res.send('This is the patch request')
+})
+
+app.delete('/api/cards', (req, res) => {
+  res.send('This is the delete request')
 })
 
 app.listen(port, () => {
