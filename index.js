dd98ccb013f3e8661691bb23d9281ea3fd307848
Suad Emre Umar <suadeumar@gmail.com>
2021-09-27 15:42:22 +0200

    first installation with express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello World!')
+/* console.log('Hello World!')
 
 const http = require('http')
 
@@ -6,10 +6,26 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
+  res.setHeader('Content-Type', 'text/html')
   res.end('<h1>Hellooo</h1>')
 })
 
 server.listen(port, () => {
   console.log(`es funktioniert im port ${port}`)
+})
+ */
+
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/html; charset=utf-8')
+  res.send('<h1>Hello World! Wie Läuft</h1>')
+  /* res.send('<h1>Hello World!</h1>') */
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
 })
